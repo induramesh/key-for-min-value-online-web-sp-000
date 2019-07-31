c60ca86066f81c7fd4d70e1a3eaa0a60cc627791
Indu Ramesh <induramesh@gmail.com>
2019-07-31 01:58:13 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,6 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  min = name_hash[0]
-  name_hash.s
+  min = name_hash.first
+  hash.select
 end
